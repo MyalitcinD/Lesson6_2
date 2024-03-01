3e6fb2ca3e819198bee7ccb4d98421b507263d7f
Денис <mden1996.07@gmail.com>
2024-03-02 02:18:01 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] string_array = s.Split();
+            string res = string_array[0];
+            foreach(string c in string_array) {
+                if(c.Length > res.Length) {
+                    res = c;
+                }
+            }
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
